277a39fed66fe0eb6e81d376aa4d43b6fc0d4ed2
sonuprajapati9794 <prajapati979473@gmail.com>
2026-04-08 21:47:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-function firstWord(s) {
-  // your code here
+function firstWord(str) {
+  if (!str) return "";
+
+  // Trim leading spaces
+  str = str.trim();
+
+  const index = str.indexOf(" ");
+
+  // If no space found, return entire string
+  return index === -1 ? str : str.slice(0, index);
 }
-
-// Do not change the code below
-
-const s = prompt("Enter String:");
-alert(firstWord(s));
